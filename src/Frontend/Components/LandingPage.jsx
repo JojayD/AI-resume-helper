072cb1f7e48f7323a2050fd7ebb5dcafa9089531
JojayD <professional.josephd@gmail.com>
2024-04-24 21:38:20 -0700

    trying to deploy url

---
+++ src/Frontend/Components/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <>
+      <div>
+        <header>
+          <div>
+            
+          </div>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage;
